d6fd562f0e544f5621c169771344d1e7a55d7159
ElenaHoshizaki <elena.hoshizaki@gmail.com>
2014-04-03 15:27:53 -0700

    add second parallax box

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,24 @@
 // jQuery - on page load
 // ------------------------------------------
 $(document).ready(function() {
+
+  // For Parallax Background with Cursor Movement
   $('#landing-content').mousemove(function(e){
     var amountMovedX = (e.pageX * -1 / 6);
     var amountMovedY = (e.pageY * -1 / 6);
     $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
   });
 
+  // For Parallax Box1 with Cursor Movement
+  $('#landing-content').mousemove(function(e){
+    var amountMovedX = (e.pageX * 1 / 3);
+    var amountMovedY = (e.pageY * 1 / 3);
+    $('#movement1').css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
+    $('#movement1').css('left', amountMovedX);
+    $('#movement1').css('top', amountMovedY);
+  });
+
+  // For Scroll - Snap to Page
   $.fn.fullpage();
   console.log("inside");
 
